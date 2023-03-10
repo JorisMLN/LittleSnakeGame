900a9087618a48d6fe2ecde6bc4f66e7dd56056f
JorisMLN <monleon.joris@gmail.com>
2023-03-10 21:10:29 +0100

    0.0.2 Init method

--- index.c
+++ index.c
@@ -5,27 +5,38 @@
 #include <windows.h>
 
 //-----------------------------------------------------------------------------//
-// Constante
+// Constantes
 #define BOARDWIDTH 40
 #define BOARDHEIGHT 40
 
-
 //-----------------------------------------------------------------------------//
 // Structure
-
 // Struc du snake
 typedef struct
 {
-    int x, y;
+  int x, y;
 } Snake;
 
 // Struc du jeu
 typedef struct
 {
-    Snake snake[BOARDWIDTH * BOARDHEIGHT];
-    int size;
-    int foodX, foodY;
-    int score;
+  Snake snake[BOARDWIDTH * BOARDHEIGHT];
+  int size;
+  int foodX, foodY;
+  int score;
 } Game;
+
+//-----------------------------------------------------------------------------//
+// Init du jeu
+
+void gameInit(Game *game)
+{
+  game->size = 1;
+  game->snake[0].x = BOARDWIDTH / 2;
+  game->snake[0].y = BOARDHEIGHT / 2;
+  game->foodX = rand() % BOARDWIDTH;
+  game->foodY = rand() % BOARDHEIGHT;
+  game->score = 0;
+}
 
 //-----------------------------------------------------------------------------//
